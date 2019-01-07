59b5d96978210703ebc2bae67e003ebcd81e5909
LetsPythonIt <46443179+LetsPythonIt@users.noreply.github.com>
2019-01-07 16:15:04 +0530

    Update 1_1.py

--- 1_1.py
+++ 1_1.py
@@ -22,13 +22,27 @@ def check_unique(string):
     
     return True
 
+def check_unique_without_DS(string):
 
+    count = 0
+    
+    for element in string:
+        if string.count(element) > 1:
+            count += 1
+            print 'False'
+            return False
+        else:
+            continue
+    if count == 0: print 'True'
 
+    return True
 
-
-string1 = 'Afzal is a very very fat friend of mine.'
+# Testcases
+string1 = 'Afzal is a very very fat friend of mine'
 string2 = 'Afzal_Unique'
 
 check_unique(string1)
-
 check_unique(string2)
+
+check_unique_without_DS(string1)
+check_unique_without_DS(string2)
