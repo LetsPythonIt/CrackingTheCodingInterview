0f497a57aa6d44a5c50335c11ba6c94594ed3f77
Sarvesh Thakur <46443179+SarveshRobotics@users.noreply.github.com>
2020-01-16 17:44:48 +0530

    In C++

---
+++ Chapter 8 - Recursion and Dynamic Programming/8_7.cpp
@@ -0,0 +1,36 @@
+/*
+ * Permutations without Duplicate: Write a method to compute all
+ * permutations of a string of unique characters.
+ */
+ 
+#include<iostream>
+#include<cstring>
+
+using namespace std;
+
+void swap(char *x, char *y){
+	char t;
+	t = *x;
+	*x = *y;
+	*y = t;
+}
+
+void permute(char* a, int l, int r){
+	
+	if(l==r)
+		cout << a << endl;
+	else{
+		for(int i=l; i<=r; i++){
+			swap((a+l), (a+i));
+			permute(a, l+1, r);
+			swap((a+l), (a+i));
+		}
+	}
+}
+
+
+int main(){
+	char str[]{"ABC"};
+	permute(str, 0, strlen(str)-1);
+	return 0;
+}
